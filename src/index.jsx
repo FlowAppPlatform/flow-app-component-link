1f96f190e5561551abc6dd9d781533c7b24e7919
Mohammed Faizuddin <faiz@avantari.org>
2018-10-29 20:12:56 +0530

    initial commit flow-app-component-link

---
+++ src/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import AppComponent from 'flow-app-component';
+
+// Link Button Canvas Styles
+import './css/theme/default.css';
+
+class LinkButtonComponent extends AppComponent {
+  constructor() {
+    super();
+    const newState = {
+      properties: [
+        {
+          categoryName: 'General',
+          categoryDescription: 'Basic settings for link button.',
+          properties: [
+            {
+              id: 'text',
+              name: 'Link Button',
+              type: 'text',
+              options: {},
+              data: null,
+            },
+            {
+              id: 'page',
+              name: 'Select Page',
+              type: 'select-page',
+              options: {},
+              data: null,
+            },
+          ],
+        },
+        {
+          categoryName: 'Events',
+          categoryDescription: 'Events for the link button',
+          properties: [
+            {
+              id: 'event',
+              name: 'Events',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+          ],
+        },
+      ],
+      iconUrl: '/assets/images/link-button-component.png',
+      name: 'Link',
+      componentType: 'link',
+      parent: null,
+      allowsChildren: false,
+    };
+
+    this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
+  }
+
+  renderContent() {
+    const elemProps = this.getElementProps();
+    elemProps.style = this.getDefaultStyle() || {};
+    return (
+      <button type="button" className="button-component" aria-busy="false">
+        <span className="button-text">
+          {this.getPropertyData('text') || 'Default Button Text'}
+        </span>
+      </button>
+    );
+  }
+}
+
+export default LinkButtonComponent;

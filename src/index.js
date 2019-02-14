7202c34ecd4fc5c5a580d37b7afa78afbd52b005
Nnanyielugo <kz.onyegbule@gmail.com>
2019-02-14 18:42:17 +0100

    Add events to Events properties and add load event

--- src/index.js
+++ src/index.js
@@ -34,8 +34,22 @@ class LinkButtonComponent extends AppComponent {
           categoryDescription: 'Events for the link button',
           properties: [
             {
-              id: 'event',
-              name: 'Events',
+              id: 'click',
+              name: 'Click Event',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+            {
+              id: 'hover',
+              name: 'Hover Event',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+            {
+              id: 'load',
+              name: 'Load Event',
               type: 'graph',
               options: {},
               data: null,
@@ -53,22 +67,25 @@ class LinkButtonComponent extends AppComponent {
     this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
   }
 
-  triggerGraphEvent = () => {
-    const graphId = this.getPropertyData('event');
+  componentDidMount() {
+    this.triggerGraphEvent('load')
+  }
+
+  triggerGraphEvent = (eventId) => {
+    const graphId = this.getPropertyData(eventId);
     this.getElementProps().onEvent(graphId)
   }
 
   renderContent() {
     const elemProps = this.getElementProps();
     elemProps.style = this.getDefaultStyle() || {};
-    const graphId = this.getPropertyData('event');
     return (
       <button 
         type="button"
         className="button-component"
         aria-busy="false"
-        onClick={this.triggerGraphEvent}
-        onMouseOver={this.triggerGraphEvent}
+        onClick={() => this.triggerGraphEvent('click')}
+        onMouseOver={() => this.triggerGraphEvent('hover')}
       >
         <span className="button-text">
           {this.getPropertyData('text') || 'Default Button Text'}
